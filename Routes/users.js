458b060f38803c9e071d7721862cfec2a6d10f05
Matthew Zhang <zhangrmatthew@gmail.com>
2021-01-15 22:24:42 -0500

    add password salting and hashing

--- Routes/users.js
+++ Routes/users.js
@@ -2,53 +2,79 @@ const jwt = require("jsonwebtoken");
 const express = require('express');
 const router = express.Router();
 const userModel = require('../Models/usersSchema');
+const bcrypt = require('bcrypt');
 require('dotenv/config');
+const saltRounds = 10;
 
 router.post('/login', function(req, res){
-  const user = new userModel(req.body);
-  userModel.exists({ username: req.body.username, password: req.body.password},
-    function(err, result){
-      if(err){
-        res.send(JSON.stringify({ErrorType: "Account Validation"}));
-      }
-      else{
-        if (result){
-          const token = generateAccessToken({username : req.body.username,
-          isAdmin: req.body.isAdmin});
-          res.send(JSON.stringify({Message: 'Login Success', adminInfo : req.body.isAdmin, token}));
+    userModel.exists({ username: req.body.username },
+      function(err, result){
+        if(err){
+          res.send(JSON.stringify({ErrorType: "Account Validation"}));
         }
         else{
-          res.send(JSON.stringify({ErrorType: 'Account Does Not Exist'}));
+          if (result){
+            userModel.find({ username: req.body.username }, function(err, currAcc){
+              if(err){
+                res.send(JSON.stringify({ErrorType: "Finding Validation"}));
+              }
+              currAcc.forEach(function(acc){
+                bcrypt.compare(req.body.password, acc.password, function(err, result) {
+                  if (err){
+                    res.send(JSON.stringify({ErrorType: "Decryption"}));
+                  }
+                  if(result){
+                    const token = generateAccessToken({username : req.body.username,
+                    isAdmin: req.body.isAdmin});
+                    res.send(JSON.stringify({Message: 'Login Success', adminInfo : req.body.isAdmin, token}));
+                  }
+                  else{
+                    res.send(JSON.stringify({ErrorType: "Invalid Password"}));
+                  }
+                });
+              });
+            });
+          }
+          else{
+            res.send(JSON.stringify({ErrorType: 'Account Does Not Exist'}));
+          }
         }
-      }
-    });
+      });
 });
 
 
 router.post('/create', function(req,res){
-  const user = new userModel(req.body);
-  userModel.exists({ username: req.body.username},
-    function(err, result){
-      if(err){
-        res.send(JSON.stringify({ErrorType: "Username Validation"}));
-      }
-      else{
-        if (result){
-          res.send(JSON.stringify({ErrorType: 'Duplicate Username'}));
-        }
-        else{
-          try{
-            user.save();
-            const token = generateAccessToken({username : req.body.username,
-            isAdmin: req.body.isAdmin});
-            res.send(JSON.stringify({Message: 'Account Creation Success', adminInfo : req.body.isAdmin, token}));
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    if (err) {
+      res.send(JSON.stringify({ErrorType: "Hashing"}));
+    }
+    else{
+      req.body.password = hash;
+      const user = new userModel(req.body);
+      userModel.exists({ username: req.body.username},
+        function(err, result){
+          if(err){
+            res.send(JSON.stringify({ErrorType: "Username Validation"}));
           }
-          catch(err){
-            res.send(JSON.stringify({ErrorType: "Creation"}));
+          else{
+            if (result){
+              res.send(JSON.stringify({ErrorType: 'Duplicate Username'}));
+            }
+            else{
+              try{
+                user.save();
+                const token = generateAccessToken({username : req.body.username,
+                isAdmin: req.body.isAdmin});
+                res.send(JSON.stringify({Message: 'Account Creation Success', adminInfo : req.body.isAdmin, token}));
+              }
+              catch(err){
+                res.send(JSON.stringify({ErrorType: "Creation"}));
+              }
+            }
           }
-        }
-      }
-    });
+        });
+    }
+  });
 });
 
 
